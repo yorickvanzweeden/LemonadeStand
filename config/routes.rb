6eb5b9a32ba39b1c2bd0d029db90f5197ff381ca
Yorick van Zweeden <yorici@gmail.com>
2017-04-12 00:35:15 +0200

    Added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
+  # Error pages
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
