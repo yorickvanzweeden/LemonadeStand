7f31e62f07e27662d6d4a19b76831e6accd72a66
Yorick van Zweeden <mail@yorickvanzweeden.nl>
2017-10-10 21:03:23 +0200

    Cooks can now add products and products are saved with cook_id (user_id)

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,14 @@
 class DashboardsController < ApplicationController
 
   def show
-    if current_user.present?
-      render "show_#{current_user.role}"
-    else
+    if !current_user.present?
       raise ActionController::RoutingError.new('Not Found')
     end
+
+    if current_user.cook?
+      @products = Product.where(cook_id: current_user.id)
+    end
+
+    render "show_#{current_user.role}"
   end
 end
